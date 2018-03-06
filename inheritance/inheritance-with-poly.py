152f6b8f3a7067ed36cf724b5cf4ff3faef2aa3f
Tony Li Xu <tonylixu@gmail.com>
2018-03-05 22:41:59 -0500

    Created MP3File class

--- inheritance/inheritance-with-poly.py
+++ inheritance/inheritance-with-poly.py
@@ -1,1 +1,11 @@
-class 
+class AudioFile:
+    def __init__(self, file_name):
+        if not file_name.endswith(self.ext):
+            raise Exception("Invalid file format")
+        
+        self.file_name = file_name
+
+class MP3File(AudioFile):
+    ext = "mp3"
+    def play(self):
+        pirnt("Playing {} as mp3".format(self.file_name))
