17fd24325608db45ccfb50646e75abf280f09718
Tony Li Xu <tonylixu@gmail.com>
2018-03-05 22:17:16 -0500

    added Subclass

--- inheritance/multi-inheritance.py
+++ inheritance/multi-inheritance.py
@@ -2,6 +2,9 @@
 A demostration of multiple Python inheritance
 
 In this exmaple we call super() instead of parent class
+
+
+
 """
 class BaseClass:
     num_base_calls = 0
@@ -12,11 +15,20 @@ class BaseClass:
 class LeftSubclass(BaseClass):
     num_left_calls = 0
     def call_me(self):
+        super().call_me()
         print("Calling method on LeftSubclass")
         self.num_left_calls += 1
 
 class RightSubclass(BaseClass):
     num_right_calls = 0
     def call_me(self):
+        super().call_me()
         print("Calling method on RightSubclass")
         self.num_right_calls += 1
+
+class Subclass(LeftSubclass, RightSubclass):
+    num_sub_calls = 0
+    def call_me(self):
+        super().call_me()
+        print("Calling method on Subclass")
+        self.num_sub_calls += 1
