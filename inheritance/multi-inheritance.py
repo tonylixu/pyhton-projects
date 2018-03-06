ba9be8ec12c78871c115716948bb4c527e30aab9
Tony Li Xu <tonylixu@gmail.com>
2018-03-05 22:14:45 -0500

    Added LeftSubclass and RightSubclass

--- inheritance/multi-inheritance.py
+++ inheritance/multi-inheritance.py
@@ -9,3 +9,14 @@ class BaseClass:
         print("Calling method on BaseClass")
         self.num_base_calls += 1
 
+class LeftSubclass(BaseClass):
+    num_left_calls = 0
+    def call_me(self):
+        print("Calling method on LeftSubclass")
+        self.num_left_calls += 1
+
+class RightSubclass(BaseClass):
+    num_right_calls = 0
+    def call_me(self):
+        print("Calling method on RightSubclass")
+        self.num_right_calls += 1
