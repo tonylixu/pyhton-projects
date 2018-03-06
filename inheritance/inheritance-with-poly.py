057cbffe803be2284b6dca294d475fba9832add7
Tony Li Xu <tonylixu@gmail.com>
2018-03-05 22:50:16 -0500

    Added doc_string

--- inheritance/inheritance-with-poly.py
+++ inheritance/inheritance-with-poly.py
@@ -1,3 +1,14 @@
+"""
+Demostrate inheritance with polymorphism.
+
+Notice how the __init__ method in the parent class is able to access
+the ext class variable from different subclasses? That's polymorphism.
+If the file_name doesn't end with the correct name, it raises an exception.
+
+The fact that AudioFile doesn't actually store a reference to the ext variable
+doesn't stop it from being able to access it on the subclass.
+
+"""
 class AudioFile:
     def __init__(self, file_name):
         if not file_name.endswith(self.ext):
