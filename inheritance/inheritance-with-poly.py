ce50a98bf24be44e71c300260ce58f27f5b48ff8
Tony Li Xu <tonylixu@gmail.com>
2018-03-05 22:43:23 -0500

    Added WavFile and OggFile class

--- inheritance/inheritance-with-poly.py
+++ inheritance/inheritance-with-poly.py
@@ -9,3 +9,13 @@ class MP3File(AudioFile):
     ext = "mp3"
     def play(self):
         pirnt("Playing {} as mp3".format(self.file_name))
+
+class WavFile(AudioFile):
+    ext = "wav"
+    def play(self):
+        pirnt("Playing {} as wav".format(self.file_name))
+
+class OggFile(AudioFile):
+    ext = "ogg"
+    def play(self):
+        pirnt("Playing {} as ogg".format(self.file_name))
